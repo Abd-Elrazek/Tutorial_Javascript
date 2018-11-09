c100e0adb5ccc05fe1abc25dca88781d3e614814
Abd Elrazek <abdelrazek.n3@gmail.com>
2018-11-09 11:11:31 +0200

    introduction of javascript

---
+++ introduction.js
@@ -0,0 +1,10 @@
+(function () {
+	"use strict";
+	// Start your code here
+	function greetions(your_name) {
+		alert('Your name is : ' + your_name);
+		console.log('your name is : ' + your_name)
+	}
+	greetions('abdelrazek');
+	// End your code here
+})();
