301fa8dadfc8464e2bf1f4437a643800802a8ec7
Abd Elrazek <abdelrazek.n3@gmail.com>
2018-11-10 19:50:51 +0200

    add image

--- introduction.js
+++ introduction.js
@@ -1,14 +1,5 @@
-function roughScale(base) {
-	var parsed = parseInt(base);
-	console.log(parsed);
-	if (isNaN(parsed)) {
-		return 0
-	}
-	return parsed * 100;
-}
-
-console.log(roughScale(16));
-// expected output: 1500
-
-console.log(roughScale('88'));
-// expected output: 0
+var str = 'this string \
+is broken \
+across multiple \
+lines.'
+console.log(str); // this string is broken across multiple lines.
