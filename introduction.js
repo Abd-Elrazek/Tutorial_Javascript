faf55a747e37cac10da9e1b705dd7de809217ce1
Abd Elrazek <abdelrazek.n3@gmail.com>
2018-11-13 17:51:34 +0200

    .

--- introduction.js
+++ introduction.js
@@ -1,1 +1,13 @@
-
+var i = 0;
+var n = 0;
+while (i < 5) {
+  console.log('i before if condition is : ' + i);
+  if (i == 3) {
+	  console.log("i before continue : " + i);
+	  console.log("i after continue : " + i);
+  }
+  n += i;
+  console.log(n);
+  i++;
+}
+//1,3,7,12
