f70ec10c5da056cd9aa307f0f1547205bbfc1c58
Abd Elrazek <abdelrazek.n3@gmail.com>
2018-11-16 20:38:46 +0200

    .

--- introduction.js
+++ introduction.js
@@ -1,30 +1,56 @@
-// I try to pass function in java but not work  but in javascript is okey
-/*
-function f (b){
-	return b + " -f";
+	// I try to pass function in java but not work  but in javascript is okey
+	/*
+	function f (b){
+		return b + " -f";
+	}
+
+	function map(f, a) {
+	  var result = [],i; // Create a new Array
+	  for (i = 0; i != a.length; i++)
+		result[i] = f(a[i]);
+	  return result;
+	}
+
+	var x = map(f,["name","address", "game"]);
+	console.log(x);
+	*/
+	/*
+	(function foo(i) {// i = 3
+	  if (i < 0) // 3 < 0 = false
+		return;
+	  log(i);// i = 3
+	  (function foo(i) {// i = 2
+		if (i < 0) // 2 < 0 = false
+		  return;
+		log(i);// i = 2
+		(function foo(i) {// i = 1
+		  if (i < 0) // 1 < 0 = false
+			return;
+		  log(i);// i = 1
+		  (function foo(i) {// i = 0
+			if (i < 0) // 0 < 0 = false
+			  return;
+			log(i);// i = 0
+			(function foo(i) {// i = -1
+			  if (i < 0) // -1 < 0 = true
+				return;
+			  log(i);
+			  foo(i - 1);
+			  log(i);
+			}(i - 1));
+			log(i);// i = 0
+		  }(i - 1));
+		  log(i);// i = 1
+		}(i - 1));
+		log(i);// i = 2
+	  }(i - 1));
+	  log(i);// i = 3
+	}(3));
+	*/
+var price = true;
+if (price === true){
+	var main =522;
+	console.log(main);
+}else{
+	console.log('100');
 }
-
-function map(f, a) {
-  var result = [],i; // Create a new Array
-  for (i = 0; i != a.length; i++)
-    result[i] = f(a[i]);
-  return result;
-}
-
-var x = map(f,["name","address", "game"]);
-console.log(x);
-*/
-
-function outside(x) {
-  function inside(y) {
-	  console.log(y);
-    return x + y;
-  }
-  return inside;
-}
-fn_inside = outside(3); // Think of it like: give me a function that adds 3 to whatever you give
-                        // it
-result = fn_inside(5); // returns 8
-console.log(result);
-result1 = outside(3)(5); // returns 8 , 3 for outside function and 5 for outwr function
-
