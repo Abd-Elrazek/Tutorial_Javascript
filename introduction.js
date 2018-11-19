a5182d2f84e492554cb8a058509d97c21e94066b
Abd Elrazek <abdelrazek.n3@gmail.com>
2018-11-19 11:47:02 +0200

    .

--- introduction.js
+++ introduction.js
@@ -47,10 +47,15 @@
 	  log(i);// i = 3
 	}(3));
 	*/
-var price = true;
-if (price === true){
-	var main =522;
-	console.log(main);
-}else{
-	console.log('100');
-}
+// Arrays
+
+var gasPrice = new Intl.NumberFormat('en-US',
+                        { style: 'currency', currency: 'USD',
+                          minimumFractionDigits: 3 });
+ 
+console.log(gasPrice.format(5.259)); // $5.259
+
+var hanDecimalRMBInChina = new Intl.NumberFormat('zh-CN-u-nu-hanidec',
+                        { style: 'currency', currency: 'CNY' });
+ 
+console.log(hanDecimalRMBInChina.format(1314.25)); // ? ?,???.??
